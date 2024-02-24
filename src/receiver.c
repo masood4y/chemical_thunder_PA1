f274fe52a3f2b5a4d702e53b1f8e1a8fbc005f3d
Thomas Shaw <83386554+ThomasAShaw@users.noreply.github.com>
2024-02-23 21:41:12 -0800

    Added receiver initialization + some template code.

--- src/receiver.c
+++ src/receiver.c
@@ -15,6 +15,9 @@
 
 _local static unsigned int receiver_current_state;
 
+_local static unsigned long long int receiver_write_rate;
+_local static FILE *receiver_file;
+
 
 // enum receiver_event
 // {
@@ -44,8 +47,10 @@ enum receiver_state
     Wait_inCase
 };
 
-// 
-_local void receiver_init(void);
+// Initialization.
+_local bool receiver_init(void);
+// Closing file, socket, etc.
+_local void receiver_finish(void);
 
 /* Connection Setup */
 _local void receiver_action_Wait_Connection(void);
@@ -66,13 +71,94 @@ void rrecv(unsigned short int myUDPport,
             char* destinationFile, 
             unsigned long long int writeRate) {
 
-    receiver_init();
+    if (!receiver_init(myUDPport, destinationFile, writeRate)) {
+        // Error'd out somewhere in initialization.
+        receiver_finish();
+        return;
+    }
 
     // call initialize
     // run in a while(1) with switch on states
 
+    while(true) {
+        switch(receiver_current_state) {
+            case(Wait_Connection):
+                receiver_action_Wait_Connection();
+                break;
+            case(Wait_for_Packet):
+                receiver_action_Wait_for_Packet();
+                break;
+            case(Wait_for_Pipleline):
+                receiver_action_Wait_for_Pipeline();
+                break;
+            case(Send_Fin_Ack):
+                receiver_action_Send_Fin_Ack();
+                break;
+            case(Wait_inCase):
+                receiver_action_Wait_inCase();
+                break;
+        }
+    }
 
+}
 
+bool receiver_init(unsigned short int myUDPport, 
+            char* destinationFile, 
+            unsigned long long int writeRate) {
+    // Create the UDP socket.
+    socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socket < 0) {
+        perror("Error with creating socket.");
+        return false;
+    }
+    
+    // Set the socket as non-blocking.
+    if (fcntl(socket, FSETFL, fcntl(socket, FGETFL, 0) | O_NONBLOCK)) {
+        perror("Error with setting socket flags.");
+        return false;
+    }
+
+    // Set socket address for receiving.
+    struct sockaddr_in receiver_socket_addr;
+    receiver_socket_addr.sin_family = AF_INET;
+    receiver_socket_addr.sin_port = htons(myUDPport);
+    receiver_socket_addr.sin_addr = INADDR_ANY; // no-specific IP-address
+
+    // Bind the socket to the address and port.
+    if (bind(socket, (struct sockaddr *)receiver_socket_addr, sizeof(receiver_socket_addr)) < 0) {
+        perror("Error binding to the port.");
+        return false;
+    }
+
+    // Open file for writing.
+    FILE *filePointer;
+    filePointer = fopen(destinationFile, "w");
+
+    if (filePointer == NULL) {
+        perror("Error opening file.");
+        return false;
+    }
+
+    // Set static variables.
+    receiver_file = filePointer;
+    receiver_write_rate = writeRate;
+    
+    // Set default values.
+    receiver_current_state = Wait_Connection;
+}
+
+void receiver_action_Wait_Connection(void) {
+    // Wait for packet...
+    // Listen to port
+    if (SYNC = 1 received) {
+        // set up UDP port, 
+        // set up Receive Window,
+        // send SYNC_ACK = 1 and writeRate back to destination, 
+        receiver_current_state = Wait_for_Packet;
+    } else {
+        // Otherwise we do nothing and just wait.
+        receiver_current_state = Wait_Connection;
+    }
 }
 
 int main(int argc, char** argv) {
@@ -82,6 +168,8 @@ int main(int argc, char** argv) {
     // command line.
 
     unsigned short int udpPort;
+    char* filename = NULL;
+    unsinged long long int writeRate = 0; // FIXME: this is just default (no limit), will need to be fixed later on.
 
     if (argc != 3) {
         fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
@@ -89,4 +177,7 @@ int main(int argc, char** argv) {
     }
 
     udpPort = (unsigned short int) atoi(argv[1]);
+    filename = argv[2];
+
+    rrecv(udpPort, filename, writeRate);
 }
