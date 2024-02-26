7950361a9a05b4d5f1aaa9adf59b6e137533ada7
masood4y <masood4y@gmail.com>
2024-02-25 21:04:56 -0800

    sender done i think

--- src/sender.c
+++ src/sender.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <fcntl.h>
+//TODO: change?
+#include <netdb.h> // Add this line
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,30 +16,47 @@
 #include <time.h>
 #include "our_protocol.h"
      
+// #define PROTOCOL_DATA_SIZE 1450
+// #define MAX_WINDOW_SIZE 21845
+// #define PACKET_SIZE 1450 // Just data.
 
+// struct protocol_Header
+// {
+//     /* Sync bit:7, Sync Ack bit:6, 0:5, 0:4, 0:3, 0:2, Fin bit:1, Fin ack bit:0 */
+//     uint8_t management_byte;
+
+//     /* Servers as Seq num for sender, and Ack num for Receiver */
+//     uint16_t seq_ack_num;
+// };
+
+// struct protocol_Packet
+// {
+//     struct protocol_Header header;
+//     char data[PROTOCOL_DATA_SIZE];
+// };
 
 // TODO: ensure all static variables are updated appropriately
 #define ALPHA 0.125
 #define BETA 0.25
 
-_local static unsigned int sender_current_state;
-_local static unsigned long long int bytes_left_to_send;
-_local static FILE *file_pointer;
-_local static int sockfd;
+static unsigned int sender_current_state;
+static unsigned long long int bytes_left_to_send;
+static FILE *file_pointer;
+static int sockfd;
 
-_local static uint16_t acknowledged[2];
-_local static uint16_t in_Flight[2];
+static uint16_t acknowledged[2];
+static uint16_t in_Flight[2];
 static uint16_t current_window_size;
 static double RTT_in_ms;
 static double timeoutInterval_in_ms;
 static double devRTT;
 
 
-_local const static uint16_t max_window_size = 21750; /* Set as (uint16_t / 3) */ 
+const static uint16_t max_window_size = 21750; /* Set as (uint16_t / 3) */ 
 
-_local static clock_t start, end;
-_local static double cpu_time_used_in_seconds;
-_local static double cpu_time_used_in_ms;
+static clock_t start, end;
+static double cpu_time_used_in_seconds;
+static double cpu_time_used_in_ms;
 static long long int file_offset_for_sending;
      
 
@@ -57,41 +77,45 @@ enum sender_state
 
 
 /* Initialization */
-_local void sender_init(void);
-_local bool open_file(char* filename, unsigned long long int bytesToTransfer);
+int sender_init(char* filename, unsigned long long int bytesToTransfer,
+                        char* hostname, unsigned short int hostUDPport);
+int open_file(char* filename, unsigned long long int bytesToTransfer); 
+int setup_socket(char* hostname, unsigned short int hostUDPport);
+void updateRTT(double sampleRTT);
 
 //TODO: close file, close socket
-_local void sender_finish(void);
+void sender_finish(void);
 
 /* Connection Setup */
-_local void sender_action_Start_Connection(void);
+void sender_action_Start_Connection(void);
 
 /* Send Data*/
-_local void sender_action_Send_N_Packets(void);
-_local void sender_action_Wait_for_Ack(void);
+void sender_action_Send_N_Packets(void);
+void sender_action_Wait_for_Ack(void);
+int valid_ack_num(uint16_t ack_num);
 
 /* Connection Teardown */
-_local void sender_action_Send_Fin(void);
-_local void sender_action_Wait_Fin_Ack(void);
+void sender_action_Send_Fin(void);
+void sender_action_Wait_Fin_Ack(void);
 
 
 
 /* Initialization */
-_local bool sender_init(char* filename, unsigned long long int bytesToTransfer,
+int sender_init(char* filename, unsigned long long int bytesToTransfer,
                         char* hostname, unsigned short int hostUDPport)
 {
-
+    sockfd = -1;
     /* File related initialization */
-    if (!open_file(filename, bytesToTransfer))
+    if (open_file(filename, bytesToTransfer))
     {
-        return false;
+        return -1;
     }
 
 
     /* Socket Set up for Listening and Sending to hostname */
-    if (!setup_socket(hostname, hostUDPport))
+    if (setup_socket(hostname, hostUDPport))
     {
-        return false;
+        return -1;
     }
 
     current_window_size = 1450;
@@ -154,15 +178,15 @@ _local bool sender_init(char* filename, unsigned long long int bytesToTransfer,
 
     /* Set up State machine */
     sender_current_state = Start_Connection;
-    return true;
+    return 0;
 }
 
-_local bool open_file(char* filename, unsigned long long int bytesToTransfer) 
+int open_file(char* filename, unsigned long long int bytesToTransfer) 
 {
     file_pointer = fopen(filename, "r");
     if (file_pointer == NULL) {
         fprintf(stderr, "Error: Could not open filename.\n");
-        return false;
+        return -1;
     }
     
     /* Get File Size */
@@ -180,9 +204,9 @@ _local bool open_file(char* filename, unsigned long long int bytesToTransfer)
     else {
         bytes_left_to_send = bytesToTransfer;
     }
-    return true;
+    return 0;
 }
-_local bool setup_socket(char* hostname, unsigned short int hostUDPport) {
+int setup_socket(char* hostname, unsigned short int hostUDPport) {
 
     struct sockaddr_in receiver_address;
     struct hostent *host;
@@ -192,7 +216,7 @@ _local bool setup_socket(char* hostname, unsigned short int hostUDPport) {
     host = gethostbyname(hostname);
     if (host == NULL) {
         fprintf(stderr, "Error: Could not resolve hostname.\n");
-        return false;
+        return -1;
         //exit(EXIT_FAILURE);
     }
 
@@ -200,11 +224,9 @@ _local bool setup_socket(char* hostname, unsigned short int hostUDPport) {
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Error opening socket");
-        return false;
+        return -1;
         //exit(EXIT_FAILURE);
     }
-
-    //TODO: bind the socket?
 
     /* Set up the receiver struct */
     memset(&receiver_address, 0, sizeof(receiver_address));
@@ -216,7 +238,7 @@ _local bool setup_socket(char* hostname, unsigned short int hostUDPport) {
     if (connect(sockfd, (struct sockaddr *)&receiver_address, sizeof(receiver_address)) < 0) {
         perror("Error connecting to server");
         close(sockfd);
-        return false;
+        return -1;
         // exit(EXIT_FAILURE);
     }
 
@@ -226,40 +248,34 @@ _local bool setup_socket(char* hostname, unsigned short int hostUDPport) {
     int flags = fcntl(sockfd, F_GETFL, 0);
     if (flags == -1) {
         perror("Error getting socket flags");
-        return false;
+        return -1;
         //close(sockfd);
         //exit(EXIT_FAILURE);
     }
     if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
         perror("Error setting socket to non-blocking mode");
-        return false;
+        return -1;
         //close(sockfd);
         //exit(EXIT_FAILURE);
     }
 
 
-    return true;
-
-
+    return 0;
 }
 
 void updateRTT(double sampleRTT) {
     // Update estimated RTT using new sample RTT value.
-    RTT_in_ms = (1- ALPHA) * RTT_in_ms + alpha * sampleRTT
+    RTT_in_ms = (1- ALPHA) * RTT_in_ms + ALPHA * sampleRTT;
     
     // Update "safety margin" for timeout intervals.
     devRTT = (1 - BETA) * devRTT + BETA * abs(sampleRTT - RTT_in_ms);
 
     // Update timeout interval using this newly estimated RTT and safety margin.
-    timeoutInterval_in_ms = RTT_in_ms + 4 * devRTT
+    timeoutInterval_in_ms = RTT_in_ms + 4 * devRTT;
 }
 
-
-
-
-
 /* Connection Setup */
-_local void sender_action_Start_Connection(void)
+void sender_action_Start_Connection(void)
 {
     /* send SYNC = 1 to receiver */ 
     struct protocol_Packet sync_packet;
@@ -267,9 +283,9 @@ _local void sender_action_Start_Connection(void)
     /* Sync bit:7, Sync Ack bit:6, 0:5, 0:4, 0:3, 0:2, Fin bit:1, Fin ack bit:0 */
     sync_packet.header.management_byte = 0;
     sync_packet.header.seq_ack_num = 0;
-    sync_packet.header.management_byte = management_byte | 0x80;
+    sync_packet.header.management_byte = sync_packet.header.management_byte | 0x80;
 
-    ssize_t bytes_sent = send(sockfd, &sync_packet, sizeof(protocol_Packet), 0);
+    ssize_t bytes_sent = send(sockfd, &sync_packet, sizeof(struct protocol_Packet), 0);
 
     if (bytes_sent < 0) {
         perror("Error sending data");
@@ -293,8 +309,8 @@ _local void sender_action_Start_Connection(void)
             if ((receive_buffer.management_byte & 0x40) == 0x40) {
                 //TODO: set up sliding window, current packet size, RTT?
                 RTT_in_ms = cpu_time_used_in_ms;
-                devRTT = RRT_in_ms/2;
-                timeoutInterval_in_ms = RTT_in_ms + 4 * devRTT
+                devRTT = RTT_in_ms /2;
+                timeoutInterval_in_ms = RTT_in_ms + 4 * devRTT;
                 sender_current_state = Send_N_Packets;
                 break;
             }
@@ -325,12 +341,12 @@ _local void sender_action_Start_Connection(void)
 
 
 /* Send Data*/
-_local void sender_action_Send_N_Packets(void) 
+void sender_action_Send_N_Packets(void) 
 {
     uint16_t sending_index;
     struct protocol_Packet packet_being_sent;
     sending_index = in_Flight[0];
-    bool first_packet = true;
+    int first_packet = 1;
     
     if (in_Flight[1] > in_Flight[0])
     {
@@ -350,13 +366,13 @@ _local void sender_action_Send_N_Packets(void)
                     packet_being_sent.data[j] = EOF;
                 }
             }
-            ssize_t bytes_sent = send(sockfd, &packet_being_sent, sizeof(protocol_Packet), 0);
+            ssize_t bytes_sent = send(sockfd, &packet_being_sent, sizeof(struct protocol_Packet), 0);
             
             // TODO: error checking on send
             if (first_packet)
             {
                 start = clock();
-                first_packet = false;
+                first_packet = 0;
             }
         }
     }
@@ -368,7 +384,7 @@ _local void sender_action_Send_N_Packets(void)
             packet_being_sent.header.management_byte = 0;
             for (i = 0; i < 1450, (sending_index <= in_Flight[1] || sending_index >= in_Flight[0]); i++)
             {
-                protocol_Packet.data[i] = fgetc(file_pointer);
+                packet_being_sent.data[i] = fgetc(file_pointer);
                 sending_index++; 
             }
             packet_being_sent.header.management_byte = 0;
@@ -378,12 +394,12 @@ _local void sender_action_Send_N_Packets(void)
                     packet_being_sent.data[j] = EOF;
                 }
             }
-            ssize_t bytes_sent = send(sockfd, &packet_being_sent, sizeof(protocol_Packet), 0);
+            ssize_t bytes_sent = send(sockfd, &packet_being_sent, sizeof(struct protocol_Packet), 0);
             // TODO: error checking on send
             if (first_packet)
             {
                 start = clock();
-                first_packet = false;
+                first_packet = -1;
             }
         }
     }
@@ -391,7 +407,7 @@ _local void sender_action_Send_N_Packets(void)
     return;
 }
 
-_local void sender_action_Wait_for_Ack(void)
+void sender_action_Wait_for_Ack(void)
 {
     if (bytes_left_to_send == 0) {
         sender_current_state = Send_Fin;
@@ -406,7 +422,7 @@ _local void sender_action_Wait_for_Ack(void)
 
         /* Check Socket for response */
         struct protocol_Header receive_buffer;
-        ssize_t bytes_received = recv(sockfd, &receive_buffer, sizeof(protocol_Header), MSG_DONTWAIT);
+        ssize_t bytes_received = recv(sockfd, &receive_buffer, sizeof(struct protocol_Header), MSG_DONTWAIT);
         if (bytes_received > 0) 
         {
             /* If its a Valid Seq number */
@@ -417,7 +433,7 @@ _local void sender_action_Wait_for_Ack(void)
                 uint16_t old_acked = acknowledged[1];
                 acknowledged[1] = ack_num - 1;
                 bytes_left_to_send = bytes_left_to_send - (acknowledged[1] - old_acked);
-                in_Flight[0] = ack_mum;
+                in_Flight[0] = ack_num;
                 if (bytes_left_to_send == 0){
                     sender_current_state = Send_Fin;
                     break;
@@ -462,7 +478,7 @@ _local void sender_action_Wait_for_Ack(void)
                 //in_Flight[1];
                 //acknowledged[0];
                 // go to Send N Packets
-            current_window_size =/2;
+            current_window_size = current_window_size/2;
             current_window_size + 1450 - (current_window_size % 1450);
             in_Flight[1] = in_Flight[0] + current_window_size;
             acknowledged[0] = in_Flight[1] + 1;
@@ -487,19 +503,25 @@ _local void sender_action_Wait_for_Ack(void)
     
     }
     return;
-
 }
 
-_local bool valid_ack_num(uint16_t ack_num) 
+int valid_ack_num(uint16_t ack_num) 
 {
 
     if (in_Flight[0] < in_Flight[1]){
-        return ((ack_num >= in_Flight[0]) && (ack_num <= (in_Flight[1] + 1)));
+        if ((ack_num >= in_Flight[0]) && (ack_num <= (in_Flight[1] + 1))){
+            return 1;
+        }
+        return 0;
     }
 
     else if (in_Flight[0] > in_Flight[1])
     {   
-        return (ack_num >= in_Flight[0]) || (ack_num <= (in_Flight[1] + 1));
+        if ((ack_num >= in_Flight[0]) || (ack_num <= (in_Flight[1] + 1)))
+        {
+            return 1;
+        }
+        return 0;
     }
 
 }
@@ -507,7 +529,7 @@ _local bool valid_ack_num(uint16_t ack_num)
 
 
 /* Connection Teardown */
-_local void sender_action_Send_Fin(void)
+void sender_action_Send_Fin(void)
 {
     /* send FIN = 1 to receiver */ 
     struct protocol_Packet fin_packet;
@@ -515,9 +537,9 @@ _local void sender_action_Send_Fin(void)
     /* Sync bit:7, Sync Ack bit:6, 0:5, 0:4, 0:3, 0:2, Fin bit:1, Fin ack bit:0 */
     fin_packet.header.management_byte = 0;
     fin_packet.header.seq_ack_num = 0;
-    fin_packet.header.management_byte = management_byte | 0x02;
+    fin_packet.header.management_byte = fin_packet.header.management_byte | 0x02;
 
-    ssize_t bytes_sent = send(sockfd, &fin_packet, sizeof(protocol_Packet), 0);
+    ssize_t bytes_sent = send(sockfd, &fin_packet, sizeof(struct protocol_Packet), 0);
 
     if (bytes_sent < 0) {
         perror("Error sending data");
@@ -530,7 +552,7 @@ _local void sender_action_Send_Fin(void)
 }
 
 
-_local void sender_action_Wait_Fin_Ack(void)
+void sender_action_Wait_Fin_Ack(void)
 {
     // Wait_FIN_Ack: do nothing/wait
     //         if (timeout), goto: Send_FIN
@@ -543,7 +565,7 @@ _local void sender_action_Wait_Fin_Ack(void)
 
         /* Check Socket for response */
         struct protocol_Header receive_buffer;
-        ssize_t bytes_received = recv(sockfd, &receive_buffer, sizeof(protocol_Header), MSG_DONTWAIT);
+        ssize_t bytes_received = recv(sockfd, &receive_buffer, sizeof(struct protocol_Header), MSG_DONTWAIT);
         if (bytes_received > 0) 
         {
             /* If its a Fin Ack*/
@@ -579,15 +601,25 @@ _local void sender_action_Wait_Fin_Ack(void)
 }
 
 
+void sender_finish(void){
+    if (sockfd != -1) {
+        close(sockfd);
+    }
+    if (file_pointer != NULL)
+    {
+        fclose(file_pointer);
+    }
+}
+
 void rsend(char* hostname, 
             unsigned short int hostUDPport, 
             char* filename, 
             unsigned long long int bytesToTransfer) 
 {
-    if (!sender_init())
+    if (sender_init(filename, bytesToTransfer,hostname, hostUDPport))
     {   
         sender_finish();
-        return error;
+        return;
     }
 
     while (sender_current_state != sender_Done) 
@@ -624,6 +656,7 @@ void rsend(char* hostname,
         }    
     }
     sender_finish();
+    return;
 }
 
 int main(int argc, char** argv) {
@@ -645,15 +678,4 @@ int main(int argc, char** argv) {
     rsend(hostname, hostUDPport, filename, bytesToTransfer);
 
     return (EXIT_SUCCESS);
-}
-
-updateRTT(double sampleRTT) {
-    // Update estimated RTT using new sample RTT value.
-    estimatedRTT = (1- alpha) * estimatedRTT + alpha * sampleRTT;
-    
-    // Update "safety margin" for timeout intervals.
-    devRTT = (1 - beta) * devRTT + beta * abs(sampleRTT - estimatedRTT);
-
-    // Update timeout interval using this newly estimated RTT and safety margin.
-    timeoutInterval = estimatedRTT + 4 * devRTT;
 }
