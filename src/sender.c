f59869a4910815287cd6ec8119592ad0880b06b2
Thomas Shaw <83386554+ThomasAShaw@users.noreply.github.com>
2024-03-02 21:31:28 -0800

    Sender style + comments

--- src/sender.c
+++ src/sender.c
@@ -4,21 +4,16 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <fcntl.h>
-//TODO: change?
-#include <netdb.h> // Add this line
-
+#include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-
 #include <pthread.h>
 #include <errno.h>
 #include <time.h>
 #include <math.h>
 #include "our_protocol.h"
 
-
-// TODO: ensure all static variables are updated appropriately
 #define ALPHA 0.125
 #define BETA 0.25
 
@@ -35,14 +30,12 @@ static double timeoutInterval_in_ms;
 static double devRTT;
 static uint8_t timer_valid;
 
-
 static clock_t start, end;
 static double cpu_time_used_in_seconds;
 static double cpu_time_used_in_ms;
 static long long int file_offset_for_sending;
 static uint8_t duplicate_ack_count;
      
-
 enum sender_state
 {
     /* Connection Setup */
@@ -58,7 +51,7 @@ enum sender_state
     sender_Done
 };
 
-
+/* ================ Function Declarations Start ================ */
 /* Initialization */
 int sender_init(char* filename, unsigned long long int bytesToTransfer,
                         char* hostname, unsigned short int hostUDPport);
@@ -68,8 +61,7 @@ void setup_cwindow(void);
 void updateRTT(double sampleRTT);
 void handle_timeout(void);
 
-
-//TODO: close file, close socket
+/* Closing file, socket, etc. */
 void sender_finish(void);
 
 /* Connection Setup */
@@ -87,14 +79,24 @@ void increment_cwindow(void);
 void half_cwindow(void);
 void quarter_cwindow(void);
 
-
 /* Connection Teardown */
 void sender_action_Send_Fin(void);
 void sender_action_Wait_Fin_Ack(void);
+/* ================ Function Declarations END ================ */
 
-
-
-/* Initialization */
+/**
+ * @brief Initializes the sender with the specified file, transfer size, hostname, and UDP port.
+ *
+ * This function sets up the file to be sent, initializes the socket for communication
+ * with the receiver, and sets up the congestion window. It also initializes the sender's
+ * state machine.
+ *
+ * @param filename The path to the file to be sent.
+ * @param bytesToTransfer The number of bytes to transfer from the file.
+ * @param hostname The hostname or IP address of the receiver.
+ * @param hostUDPport The UDP port number of the receiver.
+ * @return Returns 0 on successful initialization, -1 on failure.
+ */
 int sender_init(char* filename, unsigned long long int bytesToTransfer,
                         char* hostname, unsigned short int hostUDPport)
 {
@@ -120,6 +122,16 @@ int sender_init(char* filename, unsigned long long int bytesToTransfer,
     return 0;
 }
 
+/**
+ * @brief Opens the file to be sent and calculates the bytes to transfer.
+ *
+ * This function opens the specified file for reading and sets the bytes_left_to_send
+ * based on the file size and the requested bytes to transfer.
+ *
+ * @param filename The path to the file to be sent.
+ * @param bytesToTransfer The number of bytes to transfer from the file.
+ * @return Returns 0 on success, -1 on failure.
+ */
 int open_file(char* filename, unsigned long long int bytesToTransfer) 
 {
     file_pointer = fopen(filename, "r");
@@ -154,6 +166,18 @@ int open_file(char* filename, unsigned long long int bytesToTransfer)
     }
     return 0;
 }
+
+/**
+ * @brief Sets up the UDP socket for communication with the receiver.
+ *
+ * This function creates a UDP socket, resolves the receiver's hostname to an IP address,
+ * and connects the socket to the receiver's address and port. It also sets the socket
+ * to non-blocking mode.
+ *
+ * @param hostname The hostname or IP address of the receiver.
+ * @param hostUDPport The UDP port number of the receiver.
+ * @return Returns 0 on success, -1 on failure.
+ */
 int setup_socket(char* hostname, unsigned short int hostUDPport) {
 
     struct sockaddr_in receiver_address;
@@ -200,7 +224,13 @@ int setup_socket(char* hostname, unsigned short int hostUDPport) {
     return 0;
 }
 
-
+/**
+ * @brief Sets up the congestion window for the sender.
+ *
+ * Initializes the congestion window size and the tracking arrays for in-flight and
+ * acknowledged packets. The window size is set to either the protocol data size or the
+ * remaining bytes to send, whichever is smaller.
+ */
 void setup_cwindow(void)
 {
     /* Initialize to 1 packet. */
@@ -215,8 +245,16 @@ void setup_cwindow(void)
     acknowledged[0] = in_Flight[1] + 1;
     acknowledged[1] = in_Flight[0] - 1;
     duplicate_ack_count = 0;
-
 }
+
+/**
+ * @brief Updates the Round-Trip Time (RTT) estimates.
+ *
+ * Uses the sample RTT value to update the estimated RTT and the deviation (devRTT),
+ * and recalculates the timeout interval accordingly.
+ *
+ * @param sampleRTT The sampled RTT value for the latest acknowledged packet.
+ */
 void updateRTT(double sampleRTT) {
     // Update estimated RTT using new sample RTT value.
     RTT_in_ms = (1- ALPHA) * RTT_in_ms + ALPHA * sampleRTT;
@@ -230,6 +268,12 @@ void updateRTT(double sampleRTT) {
     timer_valid = 0;
 }
 
+/**
+ * @brief Handles the timeout event.
+ *
+ * Doubles the timeout interval as part of the exponential backoff strategy in case
+ * of a timeout event.
+ */
 void handle_timeout(void) {
     // Exponential backoff, double the timeout interval
      timeoutInterval_in_ms *= 2;
@@ -237,8 +281,13 @@ void handle_timeout(void) {
      // devRTT *= 2;
 }
 
-
-/* Connection Setup */
+/**
+ * @brief Initiates the connection setup process by sending a SYNC packet.
+ *
+ * Sends a SYNC packet to the receiver to start the connection setup. After sending,
+ * it waits for a SYNC_ACK response from the receiver and transitions the sender's state
+ * machine based on the response.
+ */
 void sender_action_Start_Connection(void)
 {
     /* send SYNC = 1 to receiver */ 
@@ -256,7 +305,6 @@ void sender_action_Start_Connection(void)
         return;
     }
     
-
     /* Start 2 second timer */
     start = clock();
     while(1)
@@ -301,10 +349,26 @@ void sender_action_Start_Connection(void)
     }
     return;    
 }
+
+/**
+ * @brief Checks if the received packet is a valid SYNC_ACK packet.
+ *
+ * Verifies if the SYNC_ACK bit is set in the management byte of the received packet header.
+ *
+ * @param receive_buffer Pointer to the received protocol header.
+ * @return Returns 1 if it's a SYNC_ACK packet, 0 otherwise.
+ */
 int is_Sync_Ack(struct protocol_Header* receive_buffer)
 {
     return ((receive_buffer->management_byte & 0x40) == 0x40);
 }
+
+/**
+ * @brief Initializes Round-Trip Time (RTT) values based on the initial measurement.
+ *
+ * Sets the initial RTT, deviation, and timeout interval values based on the first
+ * measured RTT.
+ */
 void init_rtt(void) 
 {
     RTT_in_ms = cpu_time_used_in_ms * 6;
@@ -313,8 +377,12 @@ void init_rtt(void)
     timer_valid = 0;
 }
 
-
-/* Send Data*/
+/**
+ * @brief Handles the process of sending a number of data packets.
+ *
+ * Reads data from the file and sends packets sequentially according to the current
+ * congestion window. Updates the sender's state machine to wait for acknowledgments.
+ */
 void sender_action_Send_N_Packets(void) 
 {
     uint32_t sending_index;
@@ -354,6 +422,15 @@ void sender_action_Send_N_Packets(void)
     return;
 }
 
+/**
+ * @brief Checks if a given acknowledgment number is valid.
+ *
+ * Validates the acknowledgment number against the range of sequence numbers in the
+ * current congestion window.
+ *
+ * @param ack_num The acknowledgment number to validate.
+ * @return Returns 1 if the acknowledgment number is valid, 0 otherwise.
+ */
 int valid_ack_num(uint32_t ack_num) 
 {
 
@@ -382,6 +459,16 @@ int valid_ack_num(uint32_t ack_num)
     }
     return 0;
 }
+
+/**
+ * @brief Checks if the sending index is within the current congestion window range.
+ *
+ * Determines if a given index for sending data is within the bounds of the current
+ * congestion window.
+ *
+ * @param sending_index The index to be checked.
+ * @return Returns 1 if the index is within range, 0 otherwise.
+ */
 int sending_index_in_range(uint32_t sending_index)
 {
 
@@ -399,7 +486,13 @@ int sending_index_in_range(uint32_t sending_index)
     return 0;
 }
 
-
+/**
+ * @brief Handles the state of waiting for packet acknowledgments.
+ *
+ * Monitors for incoming acknowledgments, updates the congestion window, and handles
+ * timeout events. Transitions the sender's state machine based on received
+ * acknowledgments or timeouts.
+ */
 void sender_action_Wait_for_Ack(void)
 {
     if (bytes_left_to_send == 0) {
@@ -480,6 +573,13 @@ void sender_action_Wait_for_Ack(void)
     }
     return;
 }
+
+/**
+ * @brief Increases the current congestion window size.
+ *
+ * Expands the size of the congestion window incrementally based on successful
+ * packet transmissions, up to a maximum window size.
+ */
 void increment_cwindow(void){
     
     if (current_window_size < MAX_WINDOW_SIZE) 
@@ -496,8 +596,14 @@ void increment_cwindow(void){
     duplicate_ack_count = 0;
     in_Flight[1] = in_Flight[0] + (current_window_size - 1);
     acknowledged[0] = in_Flight[1] + 1;
-
 }
+
+/**
+ * @brief Halves the current congestion window size.
+ *
+ * Reduces the current window size by half to adjust the flow control in response to network conditions.
+ * It ensures that the window size aligns with the protocol data size and does not exceed the bytes left to send.
+ */
 void half_cwindow(void)
 {
     current_window_size = current_window_size/2;
@@ -513,6 +619,13 @@ void half_cwindow(void)
     acknowledged[0] = in_Flight[1] + 1;
     duplicate_ack_count = 0;
 }
+
+/**
+ * @brief Reduces the current congestion window size to a quarter of its size.
+ *
+ * Drastically decreases the current window size to a quarter in response to network events like triple duplicate ACKs.
+ * Adjusts the window size to align with the protocol data size and ensures it does not exceed the remaining bytes to send.
+ */
 void quarter_cwindow(void)
 {
     current_window_size = current_window_size/4;
@@ -529,7 +642,12 @@ void quarter_cwindow(void)
     duplicate_ack_count = 0;
 }
 
-/* Connection Teardown */
+/**
+ * @brief Initiates the connection teardown process by sending a FIN packet.
+ *
+ * Constructs and sends a FIN packet to the receiver to signal the end of data transmission.
+ * Transitions the sender's state to waiting for a FIN_ACK response.
+ */
 void sender_action_Send_Fin(void)
 {
     /* send FIN = 1 to receiver */ 
@@ -554,7 +672,12 @@ void sender_action_Send_Fin(void)
     return;
 }
 
-
+/**
+ * @brief Waits for a FIN_ACK packet from the receiver.
+ *
+ * In this state, the sender waits for a FIN_ACK packet indicating that the receiver has acknowledged the end of transmission.
+ * If a FIN_ACK is received or a timeout occurs, it transitions to the appropriate next state.
+ */
 void sender_action_Wait_Fin_Ack(void)
 {
     // Wait_FIN_Ack: do nothing/wait
@@ -601,7 +724,12 @@ void sender_action_Wait_Fin_Ack(void)
     return;    
 }
 
-
+/**
+ * @brief Cleans up resources used by the sender.
+ *
+ * This function closes the socket and the file associated with the sender. It is used
+ * to clean up resources before the sender shuts down.
+ */
 void sender_finish(void){
     if (sockfd != -1) {
         close(sockfd);
@@ -612,6 +740,18 @@ void sender_finish(void){
     }
 }
 
+/**
+ * @brief Main state machine function for the sender.
+ *
+ * This function initializes the sender with the specified file, transfer size, hostname,
+ * and UDP port. It then enters a state machine loop, handling various states like
+ * connection setup, data sending, waiting for acknowledgments, and connection teardown.
+ *
+ * @param hostname The hostname or IP address of the receiver.
+ * @param hostUDPport The UDP port number of the receiver.
+ * @param filename The path to the file to be sent.
+ * @param bytesToTransfer The number of bytes to transfer from the file.
+ */
 void rsend(char* hostname, 
             unsigned short int hostUDPport, 
             char* filename, 
@@ -660,6 +800,16 @@ void rsend(char* hostname,
     return;
 }
 
+/**
+ * @brief Main function for the sender application.
+ *
+ * Parses command-line arguments to set up the receiver's hostname, UDP port, file to send,
+ * and the number of bytes to transfer. Then calls the rsend function to start the sending process.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return Returns the exit status.
+ */
 int main(int argc, char** argv) {
 
     int hostUDPport;
